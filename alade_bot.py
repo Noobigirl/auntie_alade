812e7ce95d160c614e4bfd0a885424c4878d5295
Benie Gouli <gouliaudrey@gmail.com>
2025-08-22 00:06:35 +0200

    auntie can now access conversation history

--- alade_bot.py
+++ alade_bot.py
@@ -1,21 +1,41 @@
 import ollama
 
-def get_prompt():
-    pass
-
-# creatign the ollama client
-
+# creating the ollama client
 client = ollama.Client()
 
-# defining the LLM model used and the prompt
-
 model = "auntie_alade"
-prompt = "hi there"
 
-# sending the prompt to the model
+# conversation history
+history = []
 
-model_response = client.generate(model = model, prompt = prompt)
 
-print("Auntie says: ")
+def get_prompt():
+    global history
+    while True: 
+        user_prompt = input("Say something to auntie Alade: ")
+        # stoping the chat if no prompt
+        if user_prompt.strip == "":
+            
+            return
+        # adding user input to history
+        history.append({"role":"user", "content": user_prompt})
+        display_response()
 
-print(model_response.response) 
+def display_response():
+    global history
+
+    # sending full conversation history to the model
+    model_response = client.chat(model = model, messages= history)
+    reply = model_response["message"]["content"]
+
+    # adding auntie's reply to history
+    history.append({"role": "assitant", "content": reply})
+    print("Auntie says: ")
+    print(reply) 
+
+
+
+
+get_prompt()
+
+
