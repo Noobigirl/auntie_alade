bec9a9db4b1d0642023006f20f421e39828ce9c3
Benie Gouli <gouliaudrey@gmail.com>
2025-08-21 18:25:30 +0200

    installed ollama2;7b. Working on addint the nigerian flavor

---
+++ alade_bot.py
@@ -0,0 +1,18 @@
+import ollama
+
+# creatign the ollama client
+
+client = ollama.Client()
+
+# defining the LLM model used and the prompt
+
+model = "llama2:7b"
+prompt = "hi there"
+
+# sending the prompt to the model
+
+model_response = client.generate(model = model, prompt = prompt)
+
+print("Auntie says: ")
+
+print(model_response.response) 
