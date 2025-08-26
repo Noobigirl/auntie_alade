97aafc302ff9a943f656ef89976168b79cc7e288
Benie Gouli <gouliaudrey@gmail.com>
2025-08-26 11:36:51 +0200

    I added a sidebar menu

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import streamlit as st 
-st.write("testing out streamlit")
+from streamlit_option_menu import option_menu
+
+page_header = st.empty()
+
+# --- helper function 
+def change_header(new_header: str) -> None:
+    global page_header
+    page_header.empty() # erasing the previous header
+    page_header.header(new_header) # adding the new one
+
+
+# sidebar menu
+with st.sidebar: # everything that goes insid the sidebar
+
+    selected_page = option_menu(
+        menu_title = "Menu",
+        options = ["Home", "Mood tracker", "Talk to Auntie", "Settings"],
+        # icons  to add later (bootstrap)
+        default_index=0 # selects "Home" as the default page
+    )
+
+
+# main page
+if selected_page == "Home" :
+    change_header("Home Page")
+else:
+   change_header("Random Page")
+
+# st.header ("Menu")
+# page = st.radio("Go to", ["Home", "Mood tracker", "Talk to Auntie", "Settings"])
