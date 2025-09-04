a346943bd77e5c37c91d0ff36965de949a041637
Benie Gouli <gouliaudrey@gmail.com>
2025-09-04 22:51:47 +0200

    Working on light and dark themes

--- apps/Settings.py
+++ apps/Settings.py
@@ -4,6 +4,48 @@ import os
 
 DATA_FILE = "moods.csv"
 
+def apply_custom_theme():
+    theme = st.session_state.get("theme", "light")
+
+    if theme == "Dark":
+        st.markdown(
+            """
+            <style>
+                body {
+                    background-color: #1e1e1e;
+                    color: #f0f0f0;
+                }
+
+                .stApp {
+                    background-color: #1e1e1e;
+                }
+
+                .stButton>button {
+                    background-color: #DEABDE;
+                    color: black;
+
+                }
+            </style>
+            """,
+            unsafe_allow_html= True
+        )
+    else:
+        st.markdown(
+            """
+            <style>
+                body{
+                    background-color: #F7DFF7;
+                    color: black;
+
+                }
+                .stApp{
+                    background-color: #F7DFF7;
+                }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
 def app():
     st.title("Settings")
 
